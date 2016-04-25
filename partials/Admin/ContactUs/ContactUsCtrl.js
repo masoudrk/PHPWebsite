e2e6cebe6e479732d9d2a87d9a6abe9635273a6d
MasoudGit <masoud@localhost.com>
2016-04-25 14:32:28 +0430

    Add google maps to page & change some styles.

---
+++ partials/Admin/ContactUs/ContactUsCtrl.js
@@ -0,0 +1,38 @@
+﻿angular.module('myApp').controller('ContactUs',
+    function ($scope, $rootScope, $routeParams, $location, $timeout, $uibModal, Extention) {
+        $scope.pagingController = {};
+
+        $scope.acceptComment = function (c,accept) {
+
+            Extention.post("acceptComment", { CommentID: c.ID, Accepted: accept }).then(function (res) {
+                if (res) {
+                    if (res.Status == "success") {
+                        Extention.toast({ status: 'success', message: 'با موفقیت تغییر کرد!' });
+                        $scope.pagingController.update();
+                    } else {
+                        Extention.toast({ status: 'error', message: 'خطا در سرور ، دوباره تلاش کنید' });
+                    }
+                } 
+            });
+        }
+
+        $scope.showContact = function(c) {
+
+            $uibModal.open({
+                animation: true,
+                templateUrl: 'myModalContent.html',
+                controller: function ($scope,$uibModalInstance, contact) {
+                    $scope.contact = contact;
+                    $scope.close = function () {
+                        $uibModalInstance.close();
+                    }
+                },
+                size: 'md',
+                resolve: {
+                    contact: function () {
+                        return c;
+                    }
+                }
+            });
+        }
+});
